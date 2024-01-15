ea71d2a5585f8e471620e4d96d5c65655f4650f5
vabyars <vasiliyrait@gmail.com>
2024-01-15 23:27:23 +0500

    homework 3, remove PlayerModel class, не выебываюсь)

--- main.js
+++ main.js
@@ -1,48 +1,42 @@
 const $arenas = document.querySelector('.arenas');
-class PlayerModel {
-    constructor(id, name, hp, img, weapon) {
-        this.id = id;
-        this.name = name;
-        this.hp = hp;
-        this.img = img;
-        this.weapon = weapon;
-    }
 
-    attack() {
+
+const player1 = {
+    id: 1,
+    name: 'Player 1',
+    hp: 100,
+    img: './assets/players/subzero.gif',
+    weapon: ['katana'],
+    attack: function () {
+        console.log(this.name + 'Fight...');
+    }
+};
+
+const player2 = {
+    id: 2,
+    name: 'Player 2',
+    hp: 100,
+    img: './assets/players/scorpion.gif',
+    weapon: ['sword'],
+    attack: function () {
         console.log(this.name + 'Fight...');
     }
 }
-
 initGame($arenas);
+
 function initGame(arena) {
-    const playerModel1 = new PlayerModel(
-        1,
-        'Player 1',
-        100,
-        './assets/players/subzero.gif',
-        ['katana']
-    );
-
-    const playerModel2 = new PlayerModel(
-        2,
-        'Player 2',
-        100,
-        './assets/players/scorpion.gif',
-        ['sword']
-    );
-
-    const $player1 = createPlayer(playerModel1);
-    const $player2 = createPlayer(playerModel2);
+    const $player1 = createPlayer(player1);
+    const $player2 = createPlayer(player2);
     arena.appendChild($player1);
     arena.appendChild($player2);
 
     const $gameButton = document.querySelector('.button');
 
     $gameButton.addEventListener('click', () => {
-        onChangeHP(playerModel1);
-        onChangeHP(playerModel2);
+        changeHP(player1);
+        changeHP(player2);
 
-        const $fightTitle = getFightTitle(playerModel1, playerModel2);
+        const $fightTitle = getFightTitle(player1, player2);
         if ($fightTitle) {
             $gameButton.disabled = true;
             arena.appendChild($fightTitle);
@@ -50,14 +44,14 @@ function initGame(arena) {
     });
 }
 
-function createPlayer(playerModel){
+function createPlayer(player){
     const $progressBar = createElement('div', 'progressbar');
 
     const $lifeBar = createElement('div', 'life');
-    $lifeBar.style.width = `${playerModel.hp}%`;
+    $lifeBar.style.width = `${player.hp}%`;
 
     const $playerName = createElement('div', 'name');
-    $playerName.textContent = playerModel.name;
+    $playerName.textContent = player.name;
 
     $progressBar.appendChild($lifeBar);
     $progressBar.appendChild($playerName);
@@ -65,11 +59,11 @@ function createPlayer(playerModel){
     const $character = createElement('div', 'character');
 
     const $characterImg = createElement('img');
-    $characterImg.src = playerModel.img;
+    $characterImg.src = player.img;
 
     $character.appendChild($characterImg);
 
-    const $player = createElement('div', `player${playerModel.id}`);
+    const $player = createElement('div', `player${player.id}`);
     $player.appendChild($progressBar);
     $player.appendChild($character);
 
@@ -90,32 +84,32 @@ function getDamage() {
     return Math.ceil(Math.random() * 20) + 5;
 }
 
-function getFightTitle(playerModel1, playerModel2) {
-    if (playerModel1.hp > 0 && playerModel2.hp > 0) {
+function getFightTitle(player1, player2) {
+    if (player1.hp > 0 && player2.hp > 0) {
         return;
     }
 
     const $fightTitle = createElement('div', 'roundTitle');
-    if (playerModel1.hp === 0 && playerModel2.hp === 0) {
+    if (player1.hp === 0 && player2.hp === 0) {
         $fightTitle.innerText = 'Double kill';
     }
-    else if (playerModel1.hp === 0) {
-        $fightTitle.innerText = playerModel2.name + ' Win';
+    else if (player1.hp === 0) {
+        $fightTitle.innerText = player2.name + ' Win';
     }
     else {
-        $fightTitle.innerText = playerModel1.name + ' Win';
+        $fightTitle.innerText = player1.name + ' Win';
     }
 
     return $fightTitle;
 }
 
-function onChangeHP(playerModel){
-    playerModel.hp -= getDamage();
+function changeHP(player){
+    player.hp -= getDamage();
 
-    if (playerModel.hp <= 0) {
-        playerModel.hp = 0;
+    if (player.hp <= 0) {
+        player.hp = 0;
     }
 
-    const $lifeBar = document.querySelector(`.player${playerModel.id} .progressbar .life`);
-    $lifeBar.style.width = `${playerModel.hp}%`;
+    const $lifeBar = document.querySelector(`.player${player.id} .progressbar .life`);
+    $lifeBar.style.width = `${player.hp}%`;
 }
